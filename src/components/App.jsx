cae756cb6fa05db1c1728c0cbc2d12c83fdf7d3e
Dovelee <elijah.malay.dovelee@accenture.com>
2024-11-15 13:56:35 +0100

    My mapping app

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry 
+    key={emojiTerm.id}
+    emoji={emojiTerm.emoji}
+    name={emojiTerm.name}
+    description={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+     <h1>
+      <span>Emojipedia</span>
+     </h1>
+     <div className="dictionary">{emojipedia.map(createEntry)}</div>
+    </div>
+  );
+}
+
+export default App;
